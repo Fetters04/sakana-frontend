939f0280aca25349842f047cbf88ebbec8c09080
Fetters <997167305@qq.com>
2025-01-18 13:41:56 +0800

    集成Element-Plus

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    Hello, Vue!
+    <el-button type="primary" :icon="Plus">主要按钮</el-button>
+    <el-pagination
+      :page-sizes="[10, 20, 30, 40]"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="100"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+// 引入图标组件
+import {Plus} from '@element-plus/icons-vue';
 
 </script>
 
--- project/src/main.ts
+++ project/src/main.ts
@@ -1,4 +1,14 @@
 import {createApp} from 'vue';
 import App from './App.vue';
+// 引入element-plus插件
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+// @ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(ElementPlus, {
+  locale: zhCn  // 国际化配置
+});
+app.mount('#app');
