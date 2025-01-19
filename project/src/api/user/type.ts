ce5075de47fb3cfb7f7f0024662d4a450000b5ac
Fetters <997167305@qq.com>
2025-01-19 21:37:00 +0800

    API接口统一管理

---
+++ project/src/api/user/type.ts
@@ -0,0 +1,37 @@
+// 登录接口需要携带参数ts类型
+export interface LoginForm {
+  username: string,
+  password: string
+}
+
+interface dataType {
+  token: string;
+}
+
+// 登录接口返回数据类型
+export interface loginResponseData {
+  code: number,
+  data: dataType
+}
+
+interface userInfo {
+  userId: number,
+  avatar: string,
+  username: string,
+  password: string,
+  desc: string,
+  roles: string[],
+  button: string[],
+  routes: string[],
+  token: string
+}
+
+interface user {
+  checkUser: userInfo;
+}
+
+// 定义服务器返回用户信息相关的数据类型
+export interface userResponseData {
+  code: number,
+  data: user
+}
