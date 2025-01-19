777e572e1760dcd40d8aa97fc8776e3057240b6e
Fetters <997167305@qq.com>
2025-01-19 16:05:24 +0800

    axios二次封装

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <h1>测试</h1>
+    <h1>测试axios二次封装</h1>
   </div>
 </template>
 
 <script setup lang="ts">
+import request from '@/utils/request.ts';
+import {onMounted} from 'vue';
 
+onMounted(() => {
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111'
+    }
+  }).then((res => {
+    console.log(res);
+  }));
+});
 </script>
 
 <style scoped lang="scss">
-div {
-  h1 {
-    color: $color;
-  }
-}
+
 </style>
