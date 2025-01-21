7b9490742fabe09ff84966522c8ae0d5575cdcf8
Fetters <997167305@qq.com>
2025-01-21 21:09:08 +0800

    注册图标为全局组件

--- project/src/components/index.ts
+++ project/src/components/index.ts
@@ -1,9 +1,11 @@
 // 引入项目中全部的全局组件
-import type {App, Component} from 'vue';
+import type { App, Component } from 'vue';
 import SvgIcon from './SvgIcon/index.vue';
 import Pagination from './Pagination/index.vue';
+// 引入element-plus提供全部图标组件
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-const allGlobalComponent: { [name: string]: Component } = {SvgIcon, Pagination};
+const allGlobalComponent: { [name: string]: Component } = { SvgIcon, Pagination };
 
 // 对外暴露插件对象
 export default {
@@ -13,5 +15,9 @@ export default {
       // 注册为全局组件
       app.component(key, allGlobalComponent[key]);
     });
+    // 将element-plus提供图标注册为全局组件
+    for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+      app.component(key, component);
+    }
   }
 };
