7451e1e2b71dbab3c9e3fb6e9d2da5d668f986be
Fetters <997167305@qq.com>
2025-02-04 11:09:41 +0800

    添加与修改属性静态搭建

--- project/src/components/Category/index.vue
+++ project/src/components/Category/index.vue
@@ -2,17 +2,17 @@
   <el-card>
     <el-form inline>
       <el-form-item label="一级分类" style="width: 300px">
-        <el-select v-model="categoryStore.c1Id" @change="handlerFirst">
+        <el-select :disabled="!scene" v-model="categoryStore.c1Id" @change="handlerFirst">
           <el-option v-for="c1 in categoryStore.c1Arr" :key="c1.id" :label="c1.name" :value="c1.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="二级分类" style="width: 300px">
-        <el-select v-model="categoryStore.c2Id" @change="handlerSecond">
+        <el-select :disabled="!scene" v-model="categoryStore.c2Id" @change="handlerSecond">
           <el-option v-for="c2 in categoryStore.c2Arr" :key="c2.id" :label="c2.name" :value="c2.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="三级分类" style="width: 300px">
-        <el-select v-model="categoryStore.c3Id">
+        <el-select :disabled="!scene" v-model="categoryStore.c3Id">
           <el-option v-for="c3 in categoryStore.c3Arr" :key="c3.id" :label="c3.name" :value="c3.id"></el-option>
         </el-select>
       </el-form-item>
@@ -24,6 +24,7 @@
 import { onMounted } from 'vue';
 import useCategoryStore from '@/store/modules/category';
 
+// 获取分类仓库
 let categoryStore = useCategoryStore();
 
 // 分类组件挂载完毕，通知仓库发请求获取一级分类数据
@@ -52,6 +53,9 @@ const handlerSecond = () => {
   // 通知仓库获取三级分类
   categoryStore.getC3();
 };
+
+// 接收父组件传递的scene
+defineProps(['scene']);
 </script>
 
 <style scoped lang="scss">
--- project/src/views/product/attr/index.vue
+++ project/src/views/product/attr/index.vue
@@ -1,23 +1,44 @@
 <template>
   <!--三级分类全局组件-->
-  <Category/>
+  <Category :scene="scene"/>
   <el-card style="margin: 20px 0">
-    <el-button type="primary" size="large" icon="Plus" :disabled="!categoryStore.c3Id">添加属性</el-button>
-    <el-table border style="margin-top: 20px" :data="attrArr">
-      <el-table-column label="序号" type="index" align="center" width="100px"></el-table-column>
-      <el-table-column label="属性名称" align="center" width="200px" prop="attrName"></el-table-column>
-      <el-table-column label="属性值名称" align="center">
-        <template #="{row, $index}">
-          <el-tag style="margin: 5px" v-for="item in row.attrValueList" :key="item.id">{{ item.valueName }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column label="操作" align="center" width="200px">
-        <template #="{row, $index}">
-          <el-button type="success" size="small" icon="Edit"></el-button>
-          <el-button type="danger" size="small" icon="Delete"></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div v-if="scene">
+      <el-button type="primary" size="large" icon="Plus" :disabled="!categoryStore.c3Id" @click="addAttr">添加属性
+      </el-button>
+      <!-- 真实数据表格 -->
+      <el-table border style="margin-top: 20px" :data="attrArr">
+        <el-table-column label="序号" type="index" align="center" width="100px"></el-table-column>
+        <el-table-column label="属性名称" align="center" width="200px" prop="attrName"></el-table-column>
+        <el-table-column label="属性值名称" align="center">
+          <template #="{row, $index}">
+            <el-tag style="margin: 5px" v-for="item in row.attrValueList" :key="item.id">{{ item.valueName }}</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" align="center" width="200px">
+          <template #="{row, $index}">
+            <el-button type="success" size="small" icon="Edit" @click="updateAttr"></el-button>
+            <el-button type="danger" size="small" icon="Delete"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div v-else>
+      <!--展示添加和修改数据的结构-->
+      <el-form inline>
+        <el-form-item label="属性名称">
+          <el-input placeholder="请输入属性名称"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" size="default" icon="Plus">添加属性值</el-button>
+      <el-button size="default">取消</el-button>
+      <el-table border style="margin: 20px 0">
+        <el-table-column label="序号" type="index" align="center" width="100px"></el-table-column>
+        <el-table-column label="属性值名称" align="center"></el-table-column>
+        <el-table-column label="操作" align="center" width="200px"></el-table-column>
+      </el-table>
+      <el-button type="primary" size="default">保存</el-button>
+      <el-button size="default" @click="cancel">取消</el-button>
+    </div>
   </el-card>
 </template>
 
@@ -31,6 +52,8 @@ import { Attr, AttrResponseData } from '@/api/product/attr/type';
 let categoryStore = useCategoryStore();
 // 存储已有的属性和属性值
 let attrArr = ref<Attr[]>([]);
+// 定义card组件内容切换
+let scene = ref<boolean>(true);
 
 // 监听仓库三级分类ID变化
 watch(() => categoryStore.c3Id, () => {
@@ -50,6 +73,19 @@ const getAttr = async () => {
   if (result.code == 200) {
     attrArr.value = result.data;
   }
+};
+
+// 添加属性按钮回调
+const addAttr = () => {
+  scene.value = false;
+};
+// 修改属性按钮回调
+const updateAttr = () => {
+  scene.value = false;
+};
+// 取消按钮回调
+const cancel = () => {
+  scene.value = true;
 };
 </script>
 
