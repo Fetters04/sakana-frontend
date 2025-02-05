7c29867fc619666a1ae91438514ec07c5f58edad
Fetters <997167305@qq.com>
2025-02-05 22:51:27 +0800

    SPU管理模块：API与ts类型定义

--- project/src/api/product/spu/index.ts
+++ project/src/api/product/spu/index.ts
@@ -1,12 +1,38 @@
 // SPU管理模块的接口
 import request from '@/utils/request';
-import { HasSpuResponseData } from '@/api/product/spu/type';
+import {
+  AllTrademark,
+  HasSaleAttrResponseData,
+  HasSpuResponseData,
+  SaleAttrResponseData,
+  SpuHasImg
+} from '@/api/product/spu/type';
 
 enum API {
   // 获取已有的SPU数据
-  HAS_SPU_URL = '/product'
+  HAS_SPU_URL = '/product',
+  // 获取已有品牌
+  TRADEMARK_URL = '/product/baseTrademark',
+  // 获取某个SPU下的全部售卖商品图片数据
+  IMAGE_URL = '/product/spuImageList/',
+  // 获取某个SPU下的全部的已有销售属性
+  SPU_HAS_SALE_ATTR_URL = '/product/spuSaleAttrList/',
+  // 获取全部的销售属性
+  ALL_SALE_ATTR_URL = '/product/baseSaleAttrList'
 }
 
-// 获取某个三级分类下已有的SPU数据
+// 获取某个三级分类下已有的SPU数据接口
 export const reqHasSPU = (currentPage: number, pageSize: number, category3Id: number | string) =>
     request.get<any, HasSpuResponseData>(API.HAS_SPU_URL + `/${currentPage}/${pageSize}?category3Id=${category3Id}`);
+// 获取已有品牌接口
+export const reqAllTrademark = () =>
+    request.get<any, AllTrademark>(API.TRADEMARK_URL);
+// 获取某个SPU下全部商品图片接口
+export const reqSpuImageList = (spuId: number) =>
+    request.get<any, SpuHasImg>(API.IMAGE_URL + spuId);
+// 获取某个SPU已有的销售属性接口
+export const reqSpuHasSaleAttr = (spuId: number) =>
+    request.get<any, SaleAttrResponseData>(API.SPU_HAS_SALE_ATTR_URL + spuId);
+// 获取全部销售属性接口
+export const reqAllSaleAttr = () =>
+    request.get<any, HasSaleAttrResponseData>(API.ALL_SALE_ATTR_URL);
--- project/src/api/product/spu/type.ts
+++ project/src/api/product/spu/type.ts
@@ -16,17 +16,76 @@ export interface SpuData {
   spuImageList: null;
 }
 
-// 统一存放SPU数据
-export type Records = SpuData[];
-
 // 获取已有的SPU接口返回数据的ts类型
 export interface HasSpuResponseData extends ResponseData {
   data: {
-    records: Records;
+    records: SpuData[];
     total: number;
     size: number;
     current: number;
     pages: number;
     searchCount: boolean;
   };
+}
+
+// 品牌数据的ts类型
+export interface Trademark {
+  id: number;
+  tmName: string;
+  logoUrl: string;
+}
+
+// 品牌接口返回数据ts类型
+export interface AllTrademark extends ResponseData {
+  data: Trademark[];
+}
+
+// 商品图片的ts类型
+export interface SpuImage {
+  id: number;
+  spuId: number;
+  imgName: string;
+  imgUrl: string;
+  createTime: string;
+  updateTime: string;
+}
+
+// 获取已有SPU照片墙接口返回数据ts类型
+export interface SpuHasImg extends ResponseData {
+  data: SpuImage[];
+}
+
+// 已有的SPU的销售属性值对象ts类型
+export interface SaleAttrValue {
+  id?: number;
+  spuId: number;
+  baseSaleAttrId: number;
+  saleAttrValueName: string;
+  saleAttrName: string;
+  isChecked: null;
+}
+
+// 销售属性对象ts类型
+export interface SaleAttr {
+  id?: number;
+  spuId: number;
+  baseSaleAttrId: number;
+  saleAttrName: string;
+  spuSaleAttrValueList: SaleAttrValue[];
+}
+
+// SPU已有销售属性接口返回数据ts类型
+export interface SaleAttrResponseData extends ResponseData {
+  data: SaleAttr[];
+}
+
+// 获取已有的销售属性的ts类型
+export interface HasSaleAttr {
+  id: number;
+  name: string;
+}
+
+// 获取全部销售属性接口返回数据ts类型
+export interface HasSaleAttrResponseData extends ResponseData {
+  data: HasSaleAttr[];
 }
