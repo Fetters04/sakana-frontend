955077523bcfb5b24b564a1cbe0ce864b1416599
Fetters <997167305@qq.com>
2025-02-26 15:02:29 +0800

    角色管理模块：添加|修改角色的对话框静态搭建

--- project/src/views/acl/role/index.vue
+++ project/src/views/acl/role/index.vue
@@ -6,14 +6,14 @@
         <el-input v-model="keyword" placeholder="请输入角色的关键字" style="width: 200px"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button @click="getHasRole()" type="primary" size="default">搜索</el-button>
+        <el-button @click="getHasRole()" :disabled="!keyword" type="primary" size="default">搜索</el-button>
         <el-button @click="reset" size="default">重置</el-button>
       </el-form-item>
     </el-form>
   </el-card>
   <!-- 主体内容 -->
   <el-card style="margin: 20px 0">
-    <el-button type="primary" icon="Plus">添加角色</el-button>
+    <el-button @click="addRole" type="primary" icon="Plus">添加角色</el-button>
     <el-table border style="margin: 20px 0" :data="allRole">
       <el-table-column label="#" align="center" width="80px" type="index"></el-table-column>
       <el-table-column label="ID" align="center" prop="id"></el-table-column>
@@ -23,7 +23,7 @@
       <el-table-column label="操作" width="300px">
         <template #="{row, $index}">
           <el-button type="primary" size="small" icon="User">分配权限</el-button>
-          <el-button type="success" size="small" icon="Edit">编辑</el-button>
+          <el-button @click="updateRole(row)" type="success" size="small" icon="Edit">编辑</el-button>
           <el-button type="danger" size="small" icon="Delete">删除</el-button>
         </template>
       </el-table-column>
@@ -40,6 +40,18 @@
         @size-change="getHasRole(1)"
     />
   </el-card>
+  <!-- 添加角色|修改角色的对话框 -->
+  <el-dialog v-model="dialogVisible" title="添加角色">
+    <el-form>
+      <el-form-item label="角色名">
+        <el-input placeholder="请输入角色名"></el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <el-button @click="dialogVisible = false">取消</el-button>
+      <el-button type="primary" @click="dialogVisible = false">确定</el-button>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup lang="ts">
@@ -54,6 +66,8 @@ let total = ref<number>(0);
 let keyword = ref<string>('');
 // 存储角色分页数据
 let allRole = ref<RoleInfo[]>([]);
+// 控制对话框的显示与隐藏
+let dialogVisible = ref<boolean>(false);
 
 onMounted(() => {
   // 获取角色数据
@@ -74,6 +88,16 @@ const getHasRole = async (pager = 1) => {
 const reset = () => {
   keyword.value = '';
   getHasRole();
+};
+
+// 添加角色按钮的回调
+const addRole = () => {
+  dialogVisible.value = true;
+};
+
+// 编辑按钮的回调
+const updateRole = (row: RoleInfo) => {
+  dialogVisible.value = true;
 };
 </script>
 
